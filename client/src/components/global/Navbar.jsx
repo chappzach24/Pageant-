7dd28b907e2ccdd2dc36351ecd503dd061bc3460
noah-10 <noah.fryman.10@gmail.com>
2025-04-03 15:21:02 -0400

    Added navbar background when scrolling and small changes for responsive design

--- client/src/components/global/Navbar.jsx
+++ client/src/components/global/Navbar.jsx
@@ -1,7 +1,26 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+
 const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return(
-        <div className="navbar-wrap position-fixed w-100">
+        <div className={`navbar-wrap position-fixed w-100 ${isScrolled ? "scrolled" : ""}`}>
             <div className="navbar-contain u-container">
                 <div className="navbar-layout d-flex justify-content-between align-items-center">
                     <div className="navbar-logo">
